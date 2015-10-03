5b0d1a0b20b2e8801d143ed3ca26f22d9df55b81
JayAr23 <phaenomenaz@gmail.com>
2015-10-03 15:19:31 +0200

    Initial commit

---
+++ app/src/main/java/mindgearstudios/interactivestory/Model/Choice.java
@@ -0,0 +1,30 @@
+package mindgearstudios.interactivestory.Model;
+
+/**
+ * Created by Jan on 19.09.2015.
+ */
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public Choice (String text, int nextPage) {
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+}
